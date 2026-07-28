f2525de0a84682390df8de1b4a871616b259e0e2
Guruchandu <pathuruguruchandu@gmail.com>
2026-07-28 11:53:10 +0530

    feat: use Sivion.png logo in navbar, footer and favicon

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logo = ({ className = "h-14" }) => (
   <img
-    src="/logo.png"
+    src="/Sivion.png"
     alt="SiviOn Global Technologies"
     className={`${className} w-auto object-contain transform hover:scale-105 transition-transform duration-300`}
     style={{ background: 'transparent' }}
